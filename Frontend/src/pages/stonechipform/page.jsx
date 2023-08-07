ad25ef13e24ac6d8a2015e6edea267f7f80f3dd8
Utkarsh Nigam <webxthedev@gmail.com>
2023-08-07 21:50:05 +0530

    Update page.jsx

--- Frontend/src/pages/stonechipform/page.jsx
+++ Frontend/src/pages/stonechipform/page.jsx
@@ -48,7 +48,7 @@ export default function StoneChip() {
 	};
 
 	return (
-		<div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
+		<div className="lg:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
 			<div className="container horizontal mt-5">
 				<Stepper steps={steps} currentStep={currentStep} />
 				<div className="my-10 p-10">
